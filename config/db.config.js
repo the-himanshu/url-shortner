fd77630f671c94d8439994d37ea66e6e0258a90d
Himanshu Malik <himanshu@whozzat.com>
2022-05-18 21:45:31 +0530

    make it better

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 // declare a Database string URI
-const DB_URI = process.env.MONGO_URIconsole.log("🚀 ~ file: db.config.js ~ line 6 ~ DB_URI", DB_URI)
+const DB_URI = process.env.MONGO_URI
 
 // creating a database connection
 mongoose.connect(DB_URI, {
